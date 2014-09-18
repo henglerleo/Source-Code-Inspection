3259e83166222b0fb0b7bff356f916d521da08c0
31209475 <31209475@099093-P31S303.macklabs.br>
2014-09-17 21:38:23 -0300

    Correção da condição de loop id de 1 a 6

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -13,36 +13,23 @@ class Troco {
 
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
-        }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
+        
+        papeisMoeda[5] = new PapelMoeda(100, (int)valor/100);
+        valor %= 100;
+        
+        papeisMoeda[4] = new PapelMoeda(50, (int)valor/50);
+        valor %= 50;
+        
+        papeisMoeda[3] = new PapelMoeda(20, (int)valor/20);
+        valor %= 20;
+        
+        papeisMoeda[2] = new PapelMoeda(10, (int)valor/10);
+        valor %= 10;
+        
+        papeisMoeda[1] = new PapelMoeda(5, (int)valor/5);
+        valor %= 5;
+        
+        papeisMoeda[0] = new PapelMoeda(2, (int)valor/2);
     }
 
     public Iterator<PapelMoeda> getIterator() {
